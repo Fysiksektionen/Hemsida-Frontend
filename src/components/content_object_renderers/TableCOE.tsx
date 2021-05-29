03f6ddf37598e634403df371628b00a92c2a0bef
Hugo Save <hugo@Thinkpad.com>
2021-05-29 16:07:49 +0200

    Möjligt att ändra storleken på tabellen.

--- src/components/content_object_renderers/TableCOE.tsx
+++ src/components/content_object_renderers/TableCOE.tsx
@@ -3,10 +3,15 @@ import { TableBlock, TableCell, TableRow } from '../../types/content_objects/blo
 import { mockText } from '../../mock_data/mock_utils';
 import TextCOR from '../content_object_renderers/TextCOR';
 import { ContentTreeContext } from '../../contexts';
+import { Row, Col } from 'react-bootstrap';
 
 type TableProps = {
     table: TableBlock,
     edit: boolean
+}
+
+function cloneTable(table: TableBlock): TableBlock {
+    return JSON.parse(JSON.stringify(table));
 }
 
 function renderRow(cells: TableCell[]) {
@@ -35,20 +40,14 @@ function fillMissing(cells: TableCell[], nrOfCells: number) {
     }
 }
 
-function makeRow(length: number): TableRow {
+function makeRow(length: number): TableCell[] {
     const cells: TableCell[] = [];
     for (let i = 0; i < length; i++) {
         const text = mockText('-');
         const cell = text as TableCell;
         cells.push(cell);
     }
-    return {
-        id: -1337,
-        dbType: 'list',
-        attributes: {},
-        detailUrl: '',
-        items: cells
-    };
+    return cells;
 }
 
 export default function TableCOE(props: TableProps) {
@@ -58,15 +57,44 @@ export default function TableCOE(props: TableProps) {
     const CTDispatcher = useContext(ContentTreeContext);
     const [focused, setFocus] = useState(false);
 
+    function removeCol() {
+        const newTable: TableBlock = cloneTable(props.table);
+        newTable.attributes.width--;
+        for (let i = 0; i < newTable.items.length; i++) {
+            newTable.items[i].items.pop();
+        }
+        CTDispatcher({ id: newTable.id, value: newTable });
+    }
+
+    // TODO Funktioner för att lägga till kolumner
+    function addCol() {
+        const newTable: TableBlock = cloneTable(props.table);
+        newTable.attributes.width++;
+        for (let i = 0; i < newTable.items.length; i++) {
+            newTable.items[i].items.push(mockText('-'));
+        }
+        CTDispatcher({ id: newTable.id, value: newTable });
+    }
+
     function addRow() {
-        const newTable: TableBlock = props.table;
-        newTable.items.push(makeRow(props.table.attributes.width));
+        const newTable: TableBlock = cloneTable(props.table);
+        newTable.attributes.height++;
+        const newCells = makeRow(newTable.attributes.width);
+        const newRow: TableRow = {
+            items: newCells,
+            detailUrl: '',
+            attributes: {},
+            dbType: 'list',
+            id: 321
+        };
+        newTable.items.push(newRow);
         CTDispatcher({ id: newTable.id, value: newTable });
     }
 
     // TODO What happens when you remove the last row?
     function removeRow() {
-        const newTable: TableBlock = props.table;
+        const newTable: TableBlock = cloneTable(props.table);
+        newTable.attributes.height--;
         newTable.items.pop();
         CTDispatcher({ id: newTable.id, value: newTable });
     }
@@ -88,19 +116,35 @@ export default function TableCOE(props: TableProps) {
     return (
         <>
             <div onFocus={() => setFocus(true)} onBlur={handleBlur} tabIndex={0}>
-                <table className="table" >
-                    {JSXRows}
-                </table>
-                {(props.edit && focused) &&
-                    <>
-                        <button onClick={addRow}>
-                    Add row
-                        </button>
-                        <button onClick={removeRow}>
-                    Remove row
-                        </button>
-                    </>
-                }
+                <Row>
+                    <Col className="col-11">
+                        <table className="table" >
+                            {JSXRows}
+                        </table>
+                        {(props.edit && focused) &&
+                            <>
+                                <button onClick={addRow}>
+                            Add row
+                                </button>
+                                <button onClick={removeRow}>
+                            Remove row
+                                </button>
+                            </>
+                        }
+                    </Col>
+                    <Col className="col-1">
+                        {(props.edit && focused) &&
+                        <>
+                            <button onClick={addCol}>
+                                Add col
+                            </button>
+                            <button onClick={removeCol}>
+                                Remove col
+                            </button>
+                        </>
+                        }
+                    </Col>
+                </Row>
             </div>
         </>
     );
