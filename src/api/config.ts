f091a9d9f823b17cb9069b14373813a555bf0cd7
Oskar <3982323+oskarr@users.noreply.github.com>
2021-07-15 23:09:28 +0200

    Freshened up the devcontainer.

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,8 +1,8 @@
 export const API_VERBOSE = true;
 
 // Default json-server location is at http://localhost:3002/api
-export const API_ROOT_URL = 'http://localhost:3002/api';
+export const API_ROOT_URL = 'http://localhost:8080/api';
 
 // Set to true if the response is expected to be on the APIResponse<T> format, and false if it's on the T fomat.
 // If using json-server, set this to true.
-export const USE_APIRESPONSE_FORMAT = true;
+export const USE_APIRESPONSE_FORMAT = false;
